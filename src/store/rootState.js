594c3c9d39269ca5565e7ea9e7285822509357e8
timyiu478 <45873712+timyiu478@users.noreply.github.com>
2021-11-07 04:03:45 +0800

    Revert "Merge branch 'FrontEnd'"
    This reverts commit b9b23e53d7e240abc2e3d6849a5051e1e7def688, reversing
    changes made to 27e423199853ef9dcf03beea4f64119e17526de7.

--- src/store/rootState.js
+++
@@ -1,9 +0,0 @@
-import { initialState as globalState } from 'store/global/state';
-import { initialState as userState } from 'store/user/state';
-
-const rootState = {
-    'global': globalState,
-    'user': userState,
-};
-
-export default rootState;
--- src/store/user/constants.js
+++
@@ -1,7 +0,0 @@
-const SET_USER_INFO = 'User/SET_USER_INFO';
-const CLEAR_USER_INFO = 'User/CLEAR_USER_INFO';
-
-export default {
-    SET_USER_INFO,
-    CLEAR_USER_INFO,
-};
