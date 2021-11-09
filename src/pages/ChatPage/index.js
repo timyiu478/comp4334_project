71144ab6c1556affcf216109ad517df7c022220c
Henrylo1227 <52083095+Henrylo1227@users.noreply.github.com>
2021-11-10 00:10:13 +0800

    Chat Page IU Update
    + contact list (template)
    + use contact icon and send icon
    
    attempt to work on toggle contact list in mobile mode

--- src/pages/ChatPage/index.js
+++ src/pages/ChatPage/index.js
@@ -1,30 +1,59 @@
 import React, { useState } from 'react';
+import { withStylesPropTypes } from 'react-with-styles';
 import styles from './styles.scss';
+import { PermContactCalendar, Send } from '@material-ui/icons';
+import { debug } from 'debug';
 
 const ChatPage = () => {
+    const contactList = ['Peter','Tony','Ryan' ]; //testing
+    var contactName = contactList[1]; 
     const [inMessage, setInMessage] = useState(['hello', 'hi', 'c']);
     const [outMessage, setOutMessage] = useState(['chat', 'test', 'df']);
+    console.log("hi");
+
+    //display mobile contact list
+    function toggleMobileContactList(){
+        console.log("hi");
+    };
+
     return (
         <>
             <div className={styles.container}>
                 <div className={styles.background} />
                 <div className={styles.chat_container}>
-                    <div className={styles.chatheader}>
-                        <h1>contact</h1>
+                    <div className={styles.contactListClickable}>
+                        <PermContactCalendar onClick={toggleMobileContactList}></PermContactCalendar>
                     </div>
-                    <div className={styles.chatbody}>
-                        {inMessage.map((content) => (
-                            <p className={styles.message_in}>{content}</p>
-                        ))}
-                        {outMessage.map((content) => (
-                            <p className={styles.message_out}>{content}</p>
-                        ))}
+                    <div className={styles.chat_header}>
+                        <h1>ChatPage</h1>
                     </div>
-                    <div className={styles.chatfooter}>
-                        <form>
-                            <input type="text" name="" />
-                            <button id="send_btn">></button>
-                        </form>
+                    <div className ={styles.chat_app_container}>
+                        <div className={styles.chat_app_contactList}>
+                            <h1>contact list</h1>
+                        </div>
+                        <div className={styles.chat_app_body}>
+                            <div className={styles.chat_app_contact}>
+                                    <h2>{contactName}</h2>
+
+                            </div>
+                            <div className={styles.chat_app_msg_container}>
+                                {inMessage.map((content) => (
+                                    <p className={styles.chat_app_msg_inMsg}>{content}</p>
+                                ))}
+                                {outMessage.map((content) => (
+                                    <p className={styles.chat_app_msg_outMsg}>{content}</p>
+                                ))}
+                            </div>                                
+                            <div className={styles.chat_app_footer}>
+                                <form>
+                                    <input type="text" name="" />
+                                    <button id="send_btn">
+                                        <Send></Send>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
