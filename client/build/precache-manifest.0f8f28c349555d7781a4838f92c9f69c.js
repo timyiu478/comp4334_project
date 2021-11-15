f68140b1979502367e9c46886456e7ad0ec0085e
Jiseok-Hong <ji385@naver.com>
2021-11-16 00:02:50 +0800

    test changing login name

---
+++ client/build/precache-manifest.0f8f28c349555d7781a4838f92c9f69c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504e67d895a6c53c496c7d634ed20e30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1a3c729eaee83e998c",
+    "url": "/static/css/main.55d233d7.chunk.css"
+  },
+  {
+    "revision": "a4c3c66ef55d2097d454",
+    "url": "/static/js/2.cdf132f7.chunk.js"
+  },
+  {
+    "revision": "2e1a3c729eaee83e998c",
+    "url": "/static/js/main.fd777d12.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "1d801dd5a6a5e44b7503e433b3a540b5",
+    "url": "/static/media/background.1d801dd5.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,37 +1,36 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import styles from './styles.scss';
 import { useHistory } from 'react-router-dom';
 import { FormControl } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Register from 'components/Register';
 
-
-import {serializeRSAKey,gen_key_pair} from 'src/genKey.js';
+import { serializeRSAKey, gen_key_pair } from 'src/genKey.js';
 import $ from 'jquery';
 
 const Login = () => {
     const [open, setOpen] = useState(false);
     const history = useHistory();
 
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleChange = (e) => {
-        if(e.target.id == "username"){
+        if (e.target.id == 'username') {
             setUsername(e.target.value);
         }
-        if(e.target.id == "password"){
+        if (e.target.id == 'password') {
             setPassword(e.target.value);
-        } 
-    }
+        }
+    };
 
     const signUp = () => {
-        const senderRSAkey = gen_key_pair(username,password);
+        const senderRSAkey = gen_key_pair(username, password);
 
         const data = {
-            'username': username,
-            'password': password
-        }
+            username: username,
+            password: password,
+        };
 
         localStorage.setItem('SenderRSAkey', serializeRSAKey(senderRSAkey));
         localStorage.setItem('username', username);
@@ -39,20 +38,19 @@ const Login = () => {
         $.ajax({
             method: 'POST',
             dataType: 'json',
-            contentType: "application/json",
+            contentType: 'application/json',
             data: JSON.stringify(data),
-            url: "/api/login/",
-            success: function(result, statusText){
+            url: '/api/login/',
+            success: function (result, statusText) {
                 console.log(result);
                 history.push('/chatpage');
             },
-            error: function(result, statusText){
+            error: function (result, statusText) {
                 console.log(result);
 
-                alert("Invalid Username or Password.");
+                alert('Invalid Username or Password.');
             },
-        })
-
+        });
     };
 
     const onClick = () => {
@@ -71,12 +69,25 @@ const Login = () => {
                     <FormControl className={styles.form}>
                         <h1 className={styles.heading}>Login</h1>
                         <div className={styles.input}>
-                            <label htmlFor="name">Login Name</label>
-                            <input type="text" name="username" id="username" autoComplete="none" value={username} onChange={handleChange} />
+                            <label htmlFor="name">Login</label>
+                            <input
+                                type="text"
+                                name="username"
+                                id="username"
+                                autoComplete="none"
+                                value={username}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className={styles.input}>
                             <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password"  value={password} onChange={handleChange} />
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                value={password}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div className={styles.submitContainer}>
                             <Button variant="contained" onClick={signUp}>
