8c479e685e0017ede1c7480a3f5f851c1825dbe0
timyiu478 <45873712+timyiu478@users.noreply.github.com>
2021-11-18 14:01:21 +0800

    .

---
+++ client/build/precache-manifest.c161b02fc0e8bd539c7d5868f6ba9ccf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50b15197f6d47efd8611f8d9468e04f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4abf6ba006f40c8b98b",
+    "url": "/static/css/main.00c1e83d.chunk.css"
+  },
+  {
+    "revision": "c634a10e3cb6d6db851e",
+    "url": "/static/js/2.f1301887.chunk.js"
+  },
+  {
+    "revision": "a4abf6ba006f40c8b98b",
+    "url": "/static/js/main.4fccdd76.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "1d801dd5a6a5e44b7503e433b3a540b5",
+    "url": "/static/media/background.1d801dd5.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
