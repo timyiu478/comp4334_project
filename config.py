d99f779f0baef10a4114148bc538e268465d56e6
timyiu478 <45873712+timyiu478@users.noreply.github.com>
2021-11-20 03:08:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ JWT_SECRET_KEY = jwt_secret_key
 JWT_TOKEN_LOCATION = 'cookies'
 JWT_COOKIE_CSRF_PROTECT = True
 JWT_CSRF_CHECK_FORM = True
+JWT_COOKIE_SECURE = True # Controls if the secure flag should be placed on cookies created by this extension. If a cookie is marked as secure it will only be sent by the web browser over an HTTPS connection
 
 SQLALCHEMY_DATABASE_URI = database_url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
