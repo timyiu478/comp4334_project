93eaa49e4f33199d69f510786c2a54a2026aed84
timyiu478 <45873712+timyiu478@users.noreply.github.com>
2021-11-21 01:46:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ from sqlalchemy import and_,or_
 from flask_sslify import SSLify
 from flask_cors import CORS
 from flask_redis import FlaskRedis
-from gevent import monkey
-
-monkey.monkey_patch() 
 
 app = Flask(__name__,static_folder = "client/build",static_url_path="/")
 app.config.from_object('config')
@@ -269,4 +266,4 @@ async def on_message(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app,message_queue=app.config["REDIS_URL"], cors_allowed_origins='*',async_mode='gevent')
+    socketio.run(app,message_queue=app.config["REDIS_URL"], cors_allowed_origins='*',async_mode='gevent_uwsgi')
