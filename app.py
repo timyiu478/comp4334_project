6b1d46a7b680291c4e3b3363ea568db470204132
timyiu478 <45873712+timyiu478@users.noreply.github.com>
2021-11-25 17:39:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -260,8 +260,6 @@ def on_join(data):
 @jwt_required(locations=['headers','cookies'])
 def on_message(data):
 
-    access_token_cookie = request.headers.get('access_token_cookie')
-
     print("-------message--------")
     print("current user: ", current_user.username)
     print(data)
