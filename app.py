366fbf43e14c7b0484cec30e54b484ff9c02887a
timyiu478 <45873712+timyiu478@users.noreply.github.com>
2021-12-05 14:53:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,18 +30,16 @@ socketio = SocketIO(app)
 
 db.create_all()
 
-# # prevent cached responses
-# @app.after_request
-# def add_header(r):
-#     """
-#     Add headers to both force latest IE rendering engine or Chrome Frame,
-#     and also to cache the rendered page for 10 minutes.
-#     """
-#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     r.headers["Pragma"] = "no-cache"
-#     r.headers["Expires"] = "0"
-#     r.headers['Cache-Control'] = 'public, max-age=0'
-#     return r
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-XSS-Protection'] = '1; mode=block'
+
+    return r
 
 @jwt.unauthorized_loader
 def unauthorized_callback(callback):
